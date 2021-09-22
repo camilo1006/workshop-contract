42e9c997c2556cf7551645c8a3919cdc8d42372e
paola agudelo <paola.agudelor@perficient.com>
2021-09-21 21:36:43 -0500

    Add provider between Inventory and FoodPreservation

---
+++ ContractTest/ContractProviderTest/ProviderBase/TestStartup.cs
@@ -0,0 +1,15 @@
+﻿namespace ContractTest.ContractProviderTest.ProviderBase
+{
+    using FoodPreservation;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class TestStartup : Startup
+    {
+        public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ProviderStateMiddleware>();
+            base.Configure(app, env);
+        }
+    }
+}
